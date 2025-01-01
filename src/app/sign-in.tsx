bc4d74e07db5a506bafbda22c0937f8823c6ab60
YuriAtzler <atzleryuri@gmail.com>
2025-01-01 13:22:18 -0300

    feat: implements nativecn ui

--- src/app/sign-in.tsx
+++ src/app/sign-in.tsx
@@ -1,9 +1,17 @@
-import { View, Text } from "react-native";
+import { Button } from "@/components/Button";
+import { Text } from "@/components/Text";
+import { View } from "react-native";
 
 export function SignIn() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text className="text-red-600">Sign in</Text>
+    <View className="gap-3 items-center justify-center flex-1">
+      <Button variant="destructive">
+        <Text>This is a component</Text>
+      </Button>
+      <Text>To install new nativecn components, write:</Text>
+      <Text className="bg-gray-200 px-3 py-1 rounded">
+        npx nativecn add [component] -c "src"
+      </Text>
     </View>
   );
 }
