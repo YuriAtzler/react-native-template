e49f11f85ed891428c0e2fdfed0c68843190d35c
YuriAtzler <atzleryuri@gmail.com>
2025-01-01 12:52:45 -0300

    feat: create a initial template with react native and nativewind

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "@/styles/global.css";
+
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { SignIn } from "@/app/sign-in";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <>
+      <SignIn />
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/app/sign-in.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export function SignIn() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text className="text-red-600">Sign in</Text>
+    </View>
+  );
+}
